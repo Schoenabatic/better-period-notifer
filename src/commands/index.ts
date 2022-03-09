f1060e0ef31a0a06d7d38bcf34d2d4f2ff4ab69e
Syncxv <aruldevarackal1@gmail.com>
2022-03-09 10:19:45 +0530

    im beyond stupid

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,8 @@
 import { Config } from './config'
 import { Ping } from './ping'
+import { Send } from './sendTest'
 import { Status } from './status'
 import { Timetable } from './timetable'
 import { Toggle } from './toggle'
 
-export default [Ping, Toggle, Timetable, Status, Config]
+export default [Ping, Toggle, Timetable, Status, Config, Send]
