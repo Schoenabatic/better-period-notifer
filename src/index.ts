35714829811e8ea738b2a9bc35236de286d82eee
Schoenabatic <87335352+Schoenabatic@users.noreply.github.com>
2022-07-16 07:12:06 +0100

    very important change
    this will make your bot way faster 100% no scam time complexity of O(n) gg ez, no need to thank me

--- src/index.ts
+++ src/index.ts
@@ -12,46 +12,46 @@ import { getState } from './util/json'
 import { keepAlive } from './util/keepalive'
 import { toggle } from './util/toggleNotifer'
 
-const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
-})
+// const client = new Client({
+//     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+// })
 
-const rest = new REST({ version: '9' }).setToken(process.env.token!)
+// const rest = new REST({ version: '9' }).setToken(process.env.token!)
 
-const main = async () => {
-    const state = getState()
-    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, state.GUILD_ID), {
-        body: commands
-    })
-    const commandNames = commands.map(s => s.name)
-    client.on('ready', () => {
-        console.log(`Logged in as ${client.user!.tag}!`)
-        notifer.init(client)
-        if (state.enabled) notifer.start()
-    })
+// const main = async () => {
+//     const state = getState()
+//     await rest.put(Routes.applicationGuildCommands(CLIENT_ID, state.GUILD_ID), {
+//         body: commands
+//     })
+//     const commandNames = commands.map(s => s.name)
+//     client.on('ready', () => {
+//         console.log(`Logged in as ${client.user!.tag}!`)
+//         notifer.init(client)
+//         if (state.enabled) notifer.start()
+//     })
 
-    client.on('interactionCreate', async interaction => {
-        if (interaction.isCommand()) {
-            const index = commandNames.indexOf(interaction.commandName)
-            if (index != -1) {
-                commands[index].execute(interaction)
-            }
-        }
-        if (interaction.isButton()) {
-            switch (interaction.customId) {
-                case 'toggle':
-                    const enabled = toggle()
-                    const row = new MessageActionRow().addComponents(
-                        new MessageButton()
-                            .setCustomId('toggle')
-                            .setLabel(enabled ? 'Disable' : 'Enable')
-                            .setStyle(enabled ? 'DANGER' : 'SUCCESS')
-                    )
-                    interaction.update({
-                        embeds: getStatusEmbed(client),
-                        components: [row]
-                    })
-                    break
+//     client.on('interactionCreate', async interaction => {
+//         if (interaction.isCommand()) {
+//             const index = commandNames.indexOf(interaction.commandName)
+//             if (index != -1) {
+//                 commands[index].execute(interaction)
+//             }
+//         }
+//         if (interaction.isButton()) {
+//             switch (interaction.customId) {
+//                 case 'toggle':
+//                     const enabled = toggle()
+//                     const row = new MessageActionRow().addComponents(
+//                         new MessageButton()
+//                             .setCustomId('toggle')
+//                             .setLabel(enabled ? 'Disable' : 'Enable')
+//                             .setStyle(enabled ? 'DANGER' : 'SUCCESS')
+//                     )
+//                     interaction.update({
+//                         embeds: getStatusEmbed(client),
+//                         components: [row]
+//                     })
+//                     break
             }
         }
     })
